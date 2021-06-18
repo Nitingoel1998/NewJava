b8d4a151b23e2ea9dbc2c30e9e0c7c2fe401955f
Administrator <Administrator@BWCBSQCHEAD.EVERESTIND.COM>
2021-06-18 23:06:47 +0530

    method

---
+++ src/New_Trainee/method.java
@@ -0,0 +1,17 @@
+package New_Trainee;
+
+ class Sum{
+int sum;
+	void sum(int x, int y) {
+	sum= x+y;
+		System.out.println(sum);
+	}
+}
+class method{
+	public static void main(String[] args) {
+		
+	
+	Sum obj= new Sum();
+	obj.sum(3,4);
+}
+	}
